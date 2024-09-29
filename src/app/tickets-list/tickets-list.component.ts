73f891da98f45362647298834a61afa8657954bc
Junaid <mughaljunaidmughal136@gmail.com>
2024-09-30 00:01:30 +0500

    Show entries

--- src/app/tickets-list/tickets-list.component.ts
+++ src/app/tickets-list/tickets-list.component.ts
@@ -15,6 +15,14 @@ export class TicketsListComponent implements OnInit {
   showErrorMessage = false;
   successMessage = '';
 
+
+  //show entries waly variables
+  paginatedTickets: Ticket[] = [];
+  entriesToShow = 5;
+  entriesOptions = [5, 10, 15, 20];
+  currentPage = 1;
+  totalPages = 1;
+
   //selected tickets
   selectedTicket?: Ticket;
 
@@ -28,6 +36,57 @@ export class TicketsListComponent implements OnInit {
     this.loadTickets();
   }
 
+  
+
+  loadTickets(): void {
+    this.ticketService.getTickets().subscribe({
+      next: (data) => {
+        this.tickets = data;
+        this.filteredTickets = data;
+        this.updatePagination();
+      },
+      error: (error) => {
+        console.log('Error loading tickets', error);
+      }
+    });
+  }
+
+  // Update the paginated tickets based on the selected number of entries and current page
+
+  updatePagination(): void{
+    console.log("Update pagination");
+    this.totalPages = Math.ceil(this.tickets.length / this.entriesToShow);
+    const startIndex = (this.currentPage - 1) * this.entriesToShow;
+    const endIndex = startIndex + this.entriesToShow;
+
+    this.paginatedTickets = this.tickets.slice(startIndex, endIndex);
+  }
+
+  //Method called hota hai jab user number of entries change krta hai
+  onEntriesChange(event: any):void{
+    this.currentPage = 1; //Reset to first page
+    this.updatePagination();
+  }
+
+  //Handle pagination control
+  previousPage():void{
+    if(this.currentPage > 1){
+      this.currentPage--;
+      this.updatePagination();
+    }
+  }
+
+  nextPage():void{
+    if(this.currentPage < this.totalPages){
+      this.currentPage++;
+      this.updatePagination();
+    }
+  }
+
+
+  //Implementation of show entries of ticket
+
+
   //Modal for ticketDetails
   openDetailsModal(content: TemplateRef<any>, ticket: Ticket): void {
     this.selectedTicket = ticket;
@@ -38,38 +97,40 @@ export class TicketsListComponent implements OnInit {
   //   this.router.navigate(['/ticket-details', ticketId]);
   // }
 
-  loadTickets(): void {
-    this.ticketService.getTickets().subscribe({
-      next: (data) => {
-        this.tickets = data;
-        this.filteredTickets = data;
-      },
-      error: (error) => {
-        console.log('Error loading tickets', error);
-      }
-    });
-  }
-
   //Search tickets method
-  searchTickets():void{
-    if(this.searchTerm.trim()){
-      debugger
-      this.filteredTickets = this.tickets.filter(ticket => 
+  // searchTickets():void{
+  //   if(this.searchTerm.trim()){
+  //     debugger
+  //     this.filteredTickets = this.tickets.filter(ticket => 
+  //       ticket.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+  //     );
+  //   }
+  //   else{
+  //     this.updatePagination();
+  //     console.log("error");
+  //     this.filteredTickets = [...this.tickets];
+  //   }
+  // }
+  searchTickets(): void {
+    if (this.searchTerm) {
+      this.paginatedTickets = this.tickets.filter(ticket =>
         ticket.title.toLowerCase().includes(this.searchTerm.toLowerCase())
       );
-    }
-    else{
+    } else {
       console.log("error");
-      this.filteredTickets = [...this.tickets];
+      this.updatePagination();
     }
   }
 
+
+  //Delete Ticket
   deleteTicket(id: number): void {
     this.ticketService.deleteTicket(id).subscribe({
       next: () => {
         this.loadTickets();
         this.showErrorMessage = true;
         this.successMessage = 'Ticket deleted successfully';
+        this.updatePagination();
         setTimeout(() => {
           this.showErrorMessage = false;
         }, 3000);
