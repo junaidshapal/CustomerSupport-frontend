2fc90a4130953ab6efb9f93a327f027ac1b44916
Junaid <mughaljunaidmughal136@gmail.com>
2024-10-03 00:50:32 +0500

    Initial changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from './Services/auth.service';
+import { filter } from 'rxjs';
+import { NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,20 @@ import { AuthService } from './Services/auth.service';
 export class AppComponent {
   title = 'CustomerSupportApp';
 
-  constructor(private router: Router, public authService: AuthService) {}
+  showSidebar= true;
+
+  constructor(private router: Router, public authService: AuthService) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        // If the URL matches login or register page, hide the sidebar
+        if (event.url === '/login' || event.url === '/register') {
+          this.showSidebar = false;
+        } else {
+          this.showSidebar = true;
+        }
+      });
+  }
+ 
   
 }
