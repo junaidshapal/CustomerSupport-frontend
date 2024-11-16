d76c70376a48f98fad9eee6de3c7c9576097bf53
Junaid <mughaljunaidmughal136@gmail.com>
2024-11-16 22:57:46 +0500

    admin undo

--- src/app/Services/admin.service.ts
+++ src/app/Services/admin.service.ts
@@ -8,16 +8,16 @@ import { Observable } from 'rxjs';
 })
 export class AdminService {
 
-  private apiUrl = 'https://localhost:7077/api/Admin';
-  constructor(private http: HttpClient, private jwtHelper: JwtHelperService) { }
+  // private apiUrl = 'https://localhost:7077/api/Admin';
+  // constructor(private http: HttpClient, private jwtHelper: JwtHelperService) { }
 
-  getAllUSers():Observable<any[]>{
-    return this.http.get<any[]>(`${this.apiUrl}/get-all-users`)
-  }
-  approveUser(userId: string):Observable<any>{
-    return this.http.post(`${this.apiUrl}/approve-user/${userId}`, {});
-  }
-  blockUser(userId:string):Observable<any>{
-    return this.http.post(`${this.apiUrl}/block-user/${userId}`, {});
-  }
+  // getAllUSers():Observable<any[]>{
+  //   return this.http.get<any[]>(`${this.apiUrl}/get-all-users`)
+  // }
+  // approveUser(userId: string):Observable<any>{
+  //   return this.http.post(`${this.apiUrl}/approve-user/${userId}`, {});
+  // }
+  // blockUser(userId:string):Observable<any>{
+  //   return this.http.post(`${this.apiUrl}/block-user/${userId}`, {});
+  // }
 }
